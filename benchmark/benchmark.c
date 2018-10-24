d9d28c85f2ce80bcab33536fca5dc15a4d2c01bc
hwang75 <const5215@outlook.com>
2018-10-24 12:46:35 -0400

    fix benchmark log

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -134,8 +134,8 @@ int main(int argc, char *argv[])
     i = rand() % number_of_objects;
     mcontainer_lock(devfd, i);
     gettimeofday(&current_time, NULL);
-    fprintf(fp, "D\t%d\t%d\t%ld\t%d\t%d\t%s\n", getpid(), cid, current_time.tv_sec * 1000000 + current_time.tv_usec, i, max_size_of_objects, mapped_data);
     mcontainer_free(devfd, i);
+    fprintf(fp, "D\t%d\t%d\t%ld\t%d\t%d\t%s\n", getpid(), cid, current_time.tv_sec * 1000000 + current_time.tv_usec, i, max_size_of_objects, "delete_an_object");
     mcontainer_unlock(devfd, i);
     //fprintf(stderr, "delete complete\n");
     fprintf(stderr, "pid:%ddone.\n",getpid());
